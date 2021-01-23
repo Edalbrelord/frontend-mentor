55da54ef19bc502be366939ca5fe213addf3de8d
Stefan van der Grift <edalbrelord@gmail.com>
2021-01-23 23:56:08 +0100

    Some changes in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function Index(props) {
 
     return <div className="text-gray-300">
         <div className={styles["hero-wrapper"]}>
-            <div className="h-full align-middle flex flex-col items-center justify-center">
+            <div className="h-full align-middle flex flex-col items-center justify-around">
                 <div className={`flex flex-col items-center justify-center p-5 rounded-xl border border-gray-400 shadow-lg bg-gray-600 bg-opacity-30 ${styles.blur}`}>
                     <div className="pb-3">
                         <h1 className="text-white text-3xl">Hey, I'm Stefan!</h1>
@@ -41,11 +41,17 @@ export default function Index(props) {
                     </div>
                 </div>
 
+                <div className="">
+                    <i className="fas fa-2x fa-angle-double-down"/>
+                </div>
+
             </div>
+
         </div>
 
-        <div className="bg-white text-black">
-            <div className="container py-20 flex flex-row mx-auto justify-evenly border-b">
+        <div className="bg-white text-black container mx-auto flex flex-col justify-center text-center">
+            <h2 className="pt-20 pb-20 font-bold text-2xl">Skills</h2>
+            <div className="pb-20 flex flex-row justify-evenly border-b">
                 <div className="border border-gray-400 p-3 rounded-md flex flex-col items-center space-y-3 w-32 justify-between text-center">
                     <i className="fas fa-server fa-2x"/>
                     <h4 className="font-bold">Backend</h4>
@@ -72,8 +78,9 @@ export default function Index(props) {
             </div>
         </div>
 
-        <div className="container mx-auto flex flex-col items-center p-20 md:px-0 space-y-24">
-            <div className="w-full flex flex-col flex-wrap md:px-20 space-y-10">
+        <div className="container mx-auto flex flex-col items-center px-20 pb-20 md:px-0 space-y-20">
+            <h2 className="pt-20 font-bold text-2xl text-black">Projects</h2>
+            <div className="w-full flex flex-col flex-wrap md:px-20 space-y-20">
                 {projectPreviews}
             </div>
         </div>
